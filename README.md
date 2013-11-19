944fbe68e98504483016c47709fa66b2d9c6586e
Simon Wade <simon.wade@gmail.com>
2013-11-19 15:15:51 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dnode-object
+
+Wrap the methods of an object in an api object that you can use with dnode.
+
+That is all.
+
+No tests yet, sorry - in a hurry!
---
+++ index.js
@@ -0,0 +1,48 @@
+function DnodeObject() {}
+
+DnodeObject.prototype.wrap = function(object, options) {
+  var api = {}, 
+      proto = object.constructor.prototype,
+      prop;
+  for (prop in proto) {
+    if (typeof object[prop] === 'function' && this.include(object, prop, options)) {
+      api[prop] = this.wrapFunction(object, prop);
+    }
+  }
+  return api;
+};
+
+DnodeObject.prototype.include = function(object, prop, options) {
+  var _ref;
+  if (prop.substring(0, 1) === '_' || prop === 'constructor') {
+    return false;
+  }
+  if (options != null) {
+    if (options.exclude && options.exclude.indexOf(prop)) {
+      return false;
+    }
+    if (options.include) {
+      if (typeof options.include === 'function') {
+        if (!options.include(prop)) {
+          return false;
+        }
+      }
+      if (options.include.indexOf(prop) === -1) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+DnodeObject.prototype.wrapFunction = function(object, prop) {
+  return function() {
+    var args, cb, rv;
+    args = Array.prototype.slice.call(arguments);
+    cb = args.pop();
+    rv = object[prop].apply(object, args);
+    return cb(rv);
+  };
+};
+
+module.exports = new DnodeObject;
